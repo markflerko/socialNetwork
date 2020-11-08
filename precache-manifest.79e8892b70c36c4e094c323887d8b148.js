57ffd54e1a0a637e046ac29f44ce66b12fb122a5
markflerko <markflerko@gmail.com>
2020-11-08 21:23:45 +0300

    Updates

---
+++ precache-manifest.79e8892b70c36c4e094c323887d8b148.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927beff3f12465f564a03eba5cd37d14",
+    "url": "/socialNetwork/index.html"
+  },
+  {
+    "revision": "a6644ffec1c11350d9c8",
+    "url": "/socialNetwork/static/css/main.3e00491d.chunk.css"
+  },
+  {
+    "revision": "c587937ef092f30ec914",
+    "url": "/socialNetwork/static/js/2.52be56b4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/socialNetwork/static/js/2.52be56b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6644ffec1c11350d9c8",
+    "url": "/socialNetwork/static/js/main.31b75a95.chunk.js"
+  },
+  {
+    "revision": "5d4c3a427b4a77b77c4b",
+    "url": "/socialNetwork/static/js/runtime-main.b7cbea6a.js"
+  },
+  {
+    "revision": "9064981b80e59f1b663c17fab41563c9",
+    "url": "/socialNetwork/static/media/preloader.9064981b.svg"
+  }
+]);
